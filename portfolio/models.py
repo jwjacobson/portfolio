2fbc20d7e270f702257fd220049c1bf3800f46a9
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-10-03 10:10:14 -0400

    Add blurb field to Project model

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=50)
+    blurb = models.CharField(max_length=100, null=True, blank=True)
     description = models.TextField()
     technology = models.CharField(max_length=50)
     url = models.URLField(null=True, blank=True)
