caed3f49856a962364a8fe2d928724a5c378bc5d
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-10-03 10:04:48 -0400

    Add url field to Project model

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,3 +4,4 @@ class Project(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     technology = models.CharField(max_length=50)
+    url = models.URLField(null=True, blank=True)
