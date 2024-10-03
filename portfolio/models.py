f98c2b01d457f7ab4ce2e41e78b0a3b65b0cff65
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-10-03 14:19:48 -0400

    Add core and supporting tech fields + default description value

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,5 +3,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=50)
     blurb = models.CharField(max_length=100, null=True, blank=True)
-    description = models.TextField()
+    description = models.TextField(default="Description goes here")
+    core_tech = models.CharField(max_length=200, null=True, blank=True)
+    supporting_tech = models.CharField(max_length=200, null=True, blank=True)
     url = models.URLField(null=True, blank=True)
