a20e33b0e96a5a5a23d1c3e27b6e15e51f514ea8
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-10-03 10:11:56 -0400

    Remove technology field from Project model

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,5 +4,4 @@ class Project(models.Model):
     title = models.CharField(max_length=50)
     blurb = models.CharField(max_length=100, null=True, blank=True)
     description = models.TextField()
-    technology = models.CharField(max_length=50)
     url = models.URLField(null=True, blank=True)
