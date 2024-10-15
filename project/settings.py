aa735f1c676c431b53a7160f4d7b6305b691fcc9
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-10-15 09:57:55 -0400

    Add sqlite database to version control

--- project/settings.py
+++ project/settings.py
@@ -84,14 +84,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
