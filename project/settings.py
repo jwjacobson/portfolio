17b7cd5d03a4fe92acee1cc89d5dedfe76963650
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-10-15 10:18:18 -0400

    Use os.path.join for STATIC_ROOT and STATICFILES_DIRS assignment

--- project/settings.py
+++ project/settings.py
@@ -128,10 +128,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'theme/static',
+    os.path.join(BASE_DIR, 'theme/static'),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
