6b1fb8fbdaefa586ec1de543ea53f52a1715b5e8
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-10-16 13:01:28 -0400

    Serve images statically rather than via ImageField

--- portfolio/models.py
+++ portfolio/models.py
@@ -11,7 +11,7 @@ class Project(models.Model):
     core_tech = models.CharField(max_length=200, null=True, blank=True)
     supporting_tech = models.CharField(max_length=200, null=True, blank=True)
     url = models.URLField(null=True, blank=True)
-    screenshot = models.ImageField(upload_to=os.getenv("SCREENSHOT_DIR"), null=True, blank=True)
+    screenshot = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return f"{self.title} | {self.blurb}"
